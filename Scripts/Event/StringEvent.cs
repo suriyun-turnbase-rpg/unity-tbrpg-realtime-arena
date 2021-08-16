c54532439be3102ca2b7cccf156666c3fc335e71
Insthync <ittipon.bay@gmail.com>
2021-08-16 15:29:02 +0700

    Delete unused codes

--- Scripts/Event/StringEvent.cs
+++ Scripts/Event/StringEvent.cs
@@ -6,16 +6,5 @@ namespace RealtimeArena.Event
     [Serializable]
     public class StringEvent : UnityEvent<string>
     {
-        // Start is called before the first frame update
-        void Start()
-        {
-
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
     }
 }
