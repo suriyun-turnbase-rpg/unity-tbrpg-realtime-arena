21a4eca225d4f1eeb783d81d7fe68d4097c7539c
Insthync <ittipon.bay@gmail.com>
2021-08-16 14:25:43 +0700

    Update schema

--- Scripts/Room/LobbyRoomState.cs
+++ Scripts/Room/LobbyRoomState.cs
@@ -6,11 +6,5 @@ namespace RealtimeArena.Room
 	{
 		[Type(0, "map", typeof(MapSchema<LobbyPlayer>))]
 		public MapSchema<LobbyPlayer> players = new MapSchema<LobbyPlayer>();
-
-		[Type(1, "string")]
-		public string title = default(string);
-
-		[Type(2, "boolean")]
-		public bool hasPassword = default(bool);
 	}
 }
