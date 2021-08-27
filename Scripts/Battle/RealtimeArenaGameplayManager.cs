417a7dcc1147ebdcda0bd3354860f4e5ce0c0f3e
Insthync <ittipon.bay@gmail.com>
2021-08-27 22:32:31 +0700

    WIP battle

--- Scripts/Battle/RealtimeArenaGameplayManager.cs
+++ Scripts/Battle/RealtimeArenaGameplayManager.cs
@@ -1,9 +1,11 @@
-﻿using UnityEngine;
+﻿using RealtimeArena.Room;
+using UnityEngine;
 
 namespace RealtimeArena.Battle
 {
     public class RealtimeArenaGameplayManager : GamePlayManager
     {
+        protected int loadedFormation = 0;
 
         protected override void Awake()
         {
@@ -18,35 +20,42 @@ namespace RealtimeArena.Battle
         {
             teamAFormation.ClearCharacters();
             teamAFormation.foeFormation = teamBFormation;
-            Debug.LogError(RealtimeArenaManager.CurrentRoom.State.players[RealtimeArenaManager.CurrentRoom.SessionId].team);
-            if (RealtimeArenaManager.CurrentRoom.State.players[RealtimeArenaManager.CurrentRoom.SessionId].team == 0)
-            {
-                teamAFormation.SetFormationCharactersForArena();
-            }
-            else
-            {
-
-            }
         }
 
         protected override void SetupTeamBFormation()
         {
             teamBFormation.ClearCharacters();
             teamBFormation.foeFormation = teamAFormation;
-            if (RealtimeArenaManager.CurrentRoom.State.players[RealtimeArenaManager.CurrentRoom.SessionId].team == 1)
-            {
-                teamBFormation.SetFormationCharactersForArena();
-            }
-            else
-            {
-
-            }
         }
 
         protected override void Start()
         {
-            // Tell the server that the this client is enter the game
-            RealtimeArenaManager.Instance.SetPlayerEnterGameState();
+            foreach (var sessionId in RealtimeArenaManager.CurrentRoom.State.players.Keys)
+            {
+                GetArenaFormationCharactersAndEquipmentsForPlayer((string)sessionId, RealtimeArenaManager.CurrentRoom.State.players[(string)sessionId]);
+            }
+        }
+
+        protected void GetArenaFormationCharactersAndEquipmentsForPlayer(string sessionId, GamePlayer player)
+        {
+            GameInstance.GameService.GetArenaFormationCharactersAndEquipments(player.id, (result) =>
+            {
+                GameInstance.Singleton.OnGameServiceGetFormationCharactersAndEquipments(result);
+                if (player.team == 0)
+                {
+                    teamAFormation.SetCharacters(result.characters.ToArray());
+                }
+                else if (player.team == 1)
+                {
+                    teamBFormation.SetCharacters(result.characters.ToArray());
+                }
+                loadedFormation++;
+                if (loadedFormation >= 2)
+                {
+                    // Tell the server that the this client is enter the game
+                    RealtimeArenaManager.Instance.SetPlayerEnterGameState();
+                }
+            });
         }
 
         protected override void Update()
@@ -54,6 +63,11 @@ namespace RealtimeArena.Battle
             base.Update();
             // Fix timescale to 1
             Time.timeScale = 1;
+        }
+
+        public override void NextWave()
+        {
+            // Override to do nothing
         }
 
         public override void OnRevive()
