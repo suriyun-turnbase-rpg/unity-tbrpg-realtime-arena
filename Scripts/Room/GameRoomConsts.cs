80153c465de32764bda45c834625b9554d76b9b1
Insthync <ittipon.bay@gmail.com>
2021-08-27 17:36:42 +0700

    Improve create room and join room function

--- Scripts/Room/GameRoomConsts.cs
+++ Scripts/Room/GameRoomConsts.cs
@@ -5,6 +5,8 @@
         public const string ROOM_NAME = "GAME";
         public const string OPTION_TITLE = "title";
         public const string OPTION_PASSWORD = "password";
+        public const string OPTION_PLAYER_ID = "playerId";
+        public const string OPTION_ACCESS_TOKEN = "accessToken";
         public const int MAX_PASSWORD_LENGTH = 9;
         public const int ENTER_GAME_COUNT_DOWN = 5;
     }
