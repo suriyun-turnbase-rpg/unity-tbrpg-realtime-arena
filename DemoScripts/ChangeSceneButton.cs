b17a0147ca1eaad2ca0ed53ec186b59f1b362cbd
Insthync <ittipon.bay@gmail.com>
2021-08-27 18:08:32 +0700

    Update demo content

---
+++ DemoScripts/ChangeSceneButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RealtimeArena.DemoScripts
+{
+    public class ChangeSceneButton : MonoBehaviour
+    {
+        public string sceneName;
+        public void OnClickChangeScene()
+        {
+            GameInstance.Singleton.LoadSceneIfNotLoaded(sceneName);
+        }
+    }
+}
