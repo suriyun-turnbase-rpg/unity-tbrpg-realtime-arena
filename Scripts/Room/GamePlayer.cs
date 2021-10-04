847a8b995b5c157780862d9dd871e3dadd302bf4
Insthync <ittipon.bay@gmail.com>
2021-10-04 08:21:36 +0700

    Update schema follow server changes

--- Scripts/Room/GamePlayer.cs
+++ Scripts/Room/GamePlayer.cs
@@ -5,27 +5,30 @@ namespace RealtimeArena.Room
 	public partial class GamePlayer : Schema
 	{
 		[Type(0, "string")]
-		public string id = default(string);
+		public string sessionId = default(string);
 
 		[Type(1, "string")]
+		public string id = default(string);
+
+		[Type(2, "string")]
 		public string profileName = default(string);
 
-		[Type(2, "int32")]
+		[Type(3, "int32")]
 		public int exp = default(int);
 
-		[Type(3, "int32")]
+		[Type(4, "int32")]
 		public int teamBP = default(int);
 
-		[Type(4, "string")]
+		[Type(5, "string")]
 		public string mainCharacter = default(string);
 
-		[Type(5, "int32")]
+		[Type(6, "int32")]
 		public int mainCharacterExp = default(int);
 
-		[Type(6, "uint8")]
+		[Type(7, "uint8")]
 		public byte team = 0;
 
-		[Type(7, "uint8")]
+		[Type(8, "uint8")]
 		public byte state = 0;
 	}
 }
