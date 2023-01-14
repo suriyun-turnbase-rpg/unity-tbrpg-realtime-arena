af9cedea887d3e8d62ee5eb70c127cc503af9702
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2023-01-14 18:27:26 +0700

    feat: Implement provoke skill

--- Scripts/Battle/RealtimeArenaGameplayManager.cs
+++ Scripts/Battle/RealtimeArenaGameplayManager.cs
@@ -142,37 +142,39 @@ namespace RealtimeArena.Battle
             ActiveCharacter.DecreaseBuffsTurn();
             ActiveCharacter.DecreaseSkillsTurn();
             ActiveCharacter.ResetStates();
-            if (ActiveCharacter.Hp > 0 &&
-                !ActiveCharacter.IsStun)
+            if (ActiveCharacter.Hp <= 0 || ActiveCharacter.IsStun)
             {
-                if (ActiveCharacter.IsPlayerCharacter)
+                ActiveCharacter.NotifyEndAction();
+                return;
+            }
+            if (ActiveCharacter.IsPlayerCharacter)
+            {
+                if (ActiveCharacter.IsProvoked)
                 {
-                    if (IsAutoPlay)
-                    {
-                        ActiveCharacter.RandomAction();
-                    }
-                    else
-                    {
-                        uiCharacterActionManager.Show();
-                        waitForActionCoroutine = StartCoroutine(WaitForActionSelection());
-                    }
+                    ActiveCharacter.DoProvokedAction();
+                    return;
+                }
+                if (IsAutoPlay)
+                {
+                    ActiveCharacter.RandomAction();
                 }
                 else
                 {
-                    if (RealtimeArenaManager.CurrentRoom.State.players.Count == 1)
-                    {
-                        // Another player exit the game
-                        ActiveCharacter.RandomAction();
-                    }
-                    else
-                    {
-                        waitForActionCoroutine = StartCoroutine(WaitForActionSelection());
-                    }
+                    uiCharacterActionManager.Show();
+                    waitForActionCoroutine = StartCoroutine(WaitForActionSelection());
                 }
             }
             else
             {
-                ActiveCharacter.NotifyEndAction();
+                if (RealtimeArenaManager.CurrentRoom.State.players.Count == 1)
+                {
+                    // Another player exit the game
+                    ActiveCharacter.RandomAction();
+                }
+                else
+                {
+                    waitForActionCoroutine = StartCoroutine(WaitForActionSelection());
+                }
             }
         }
 
